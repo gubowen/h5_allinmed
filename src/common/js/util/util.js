8a2a0c39f6b60eb7b957a8063ecbd7f62706fe8a
jukun <jukun@allinmd.cn>
2017-09-13 16:13:17 +0800

    util.js 基础方法扩展 （时间处理）
    review:李晨阳

--- src/common/js/util/util.js
+++ src/common/js/util/util.js
@@ -294,6 +294,30 @@ class Api {
     }
   }
 
+  //时间处理  2017年05月08日 星期一 14:20 / 2017.05.08 星期一 14:20
+  timeFormate(op){
+    let _operationTime = ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'],
+      _timeDeal = op.time.substring(0,10).replace(/\-/g, "\/"),
+      _timesList = new Date(_timeDeal).getDay(),
+      _timeCompare = '' ,                        //  2017年05月08日 星期一 14:20
+      _timeCompares = '' ,                       //  05月08日 星期一 14:20
+      _week = _operationTime[_timesList],
+      _hours = op.time.substring(11,16);
+    switch (parseInt(op.type)) {
+      case 1:      //2017年05月08日
+        _timeCompare = op.time.substring(0, 4) + "年" + op.time.substring(5, 7) + "月" + op.time.substring(8, 10) + "日"; //  2017年05月08日 星期一 14:20
+        _timeCompares = op.time.substring(5, 7) + "月" + op.time.substring(8, 10) + "日";                                 //  05月08日 星期一 14:20
+        break;
+      case 2:      //2017.05.08
+        _timeCompare = op.time.substring(0, 4) + "." + op.time.substring(5, 7) + "." + op.time.substring(8, 10) + "日";  //  2017.05.08 星期一 14:20
+        _timeCompares = op.time.substring(5, 7) + "." + op.time.substring(8, 10) + ".";                                  //  05.08 星期一 14:20
+        break;
+      case 3:
+        break;
+    }
+    return {year:_timeCompare,years:_timeCompares,week:_week,hour:_hours};
+  }
+
   MillisecondToDate(msd) {
     var time = parseFloat(msd) / 1000;
     if (null != time && "" != time) {
