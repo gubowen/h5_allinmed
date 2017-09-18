7102b7d025ec7f47114a506f35b7a0bdc7566b0c
hallmader <hallmader@163.com>
2017-09-18 16:24:20 +0800

    解决瀑布流加载在IOS端短时间内多次请求的问题 Reviewer：李晨阳

--- src/common/js/util/util.js
+++ src/common/js/util/util.js
@@ -203,18 +203,18 @@ class Api {
     let _currentPageUrl = (window.location.origin + window.location.pathname + window.location.search),
       _encodeUrl = encodeURIComponent(_currentPageUrl);
 
-    let env = "";
+    let envCode = "";
 
     if (window.location.hostname.indexOf("m1") < 0) {
-      env = 1;
+      envCode = 1;
     } else {
-      env = 2;
+      envCode = 2;
     }
-    
-    if (env == 1) {
+
+    if (envCode == 1) {
       appId = "wxe8384f7b06c169ef";
       XHRUrl = "http://m.allinmed.cn/mcall/wx/tocure/interact/v1/view/";
-    } else if (env == 2) {
+    } else if (envCode == 2) {
       appId = "wxaa5288ad7f627608";
       XHRUrl = "http://m1.allinmed.cn/mcall/wx/tocure/interact/v1/view/";
     }
