d8e5170c46e63ad504a2b531029a039b27fff7dc
qiangkailiang <qkl200605251>
2017-09-27 16:33:07 +0800

    公共方法拆分 Reviewer：李晨阳

--- src/common/js/util/util.js
+++ src/common/js/util/util.js
@@ -7,39 +7,17 @@
  * Created by Qiangkailiang on 17/7/14.
  */
 
-import axios from "axios";
-
+import ajax from "./ajax";
+import prototype from "./prototype";
+import wxCommon from "./wxCommon";
+import accountValidate from "./accountValidate";
 class Api {
   constructor() {
 
   }
 
   ajax(param) {
-    axios.interceptors.request.use(function(config) { //在请求发出之前进行一些操作
-      if (document.querySelector(".ev-loading")) {
-        document.querySelector(".ev-loading").style.display = "block";
-      }
-      param.beforeSend && param.beforeSend(config);
-      return config;
-    });
-
-    axios({
-      url: param.url,
-      method: param.method,
-      data: param.data,
-      transformRequest: [function(data) {
-        return "paramJson=" + JSON.stringify(data);
-      }],
-      headers: { 'X-Requested-With': 'XMLHttpRequest' },
-      timeout: 30000
-    }).then((res) => {
-      param.done(res.data);
-      if (document.querySelector(".ev-loading")) {
-        document.querySelector(".ev-loading").style.display = "none";
-      }
-    }, (err) => {
-      param.fail && param.fail(err);
-    });
+    ajax(param);
   }
 
   getPara(symbol) {
@@ -62,7 +40,7 @@ class Api {
     return [...new Set(arr)];
   }
 
-  removeByValue = function(arr, value) {
+  removeByValue = function (arr, value) {
     for (let i = 0; i < this.length; i++) {
       if (arr[i] == val) {
         arr.splice(i, 1);
@@ -73,13 +51,7 @@ class Api {
   }
 
   isWXBrowse() {
-    let ua = navigator.userAgent.toLowerCase();
-    if (ua.match(/MicroMessenger/i) == "micromessenger" && ua.indexOf("iphone") > 0) {
-      return "iphoneWX";
-    } else if (ua.match(/MicroMessenger/i) == "micromessenger" && ua.indexOf("android") > 0) {
-      return "androidWX";
-    }
-    return "other";
+    wxCommon.isWXBrowse();
   }
 
   getByteLen(val) {
@@ -116,7 +88,7 @@ class Api {
   }
 
   getConnectType() {
-    let connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection || { tyep: 'unknown' };
+    let connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection || {tyep: 'unknown'};
     let type_text = ['unknown', 'ethernet', 'wifi', '2g', '3g', '4g', 'none'];
     let _browseType = this.isWXBrowse(),
       userAgentInfo = navigator.userAgent;
@@ -177,131 +149,21 @@ class Api {
 
   //openId check
   checkOpenId() {
-    let _openId = localStorage.getItem("openId"),
-      _checkKey = '';
-    if (_openId != null) {
-      _checkKey = true;
-    } else {
-      _checkKey = false;
-      if (sessionStorage.getItem("count") && sessionStorage.getItem("count").length > 0) {
-        sessionStorage.removeItem("count");
-      }
-    }
-    return _checkKey;
-  };
-
-  //get openID
-  wxGetOpenId(env) {
-    /* env环境变量参数
-     * 1代表唯医骨科-正式线上环境
-     * 2代表唯仁唯医社区-线下调试环境
-     *
-     * 08.11 目前新商户平台未申请 参数是反过来的 唯医骨科为预上线调试环境
-     */
-    let appId = "";
-    let XHRUrl = "";
-    let _currentPageUrl = (window.location.origin + window.location.pathname + window.location.search),
-      _encodeUrl = encodeURIComponent(_currentPageUrl);
-
-    let envCode = "";
-
-    if (window.location.hostname.indexOf("m1") < 0) {
-      envCode = 1;
-    } else {
-      envCode = 2;
-    }
-
-    if (envCode == 1) {
-      appId = "wxe8384f7b06c169ef";
-      XHRUrl = "http://m.allinmed.cn/mcall/wx/tocure/interact/v1/view/";
-    } else if (envCode == 2) {
-      appId = "wxaa5288ad7f627608";
-      XHRUrl = "http://m1.allinmed.cn/mcall/wx/tocure/interact/v1/view/";
-    }
-
-    let _url = "https://open.weixin.qq.com/connect/oauth2/authorize?" +
-      "appid=" + appId +
-      "&redirect_uri=" + _encodeUrl +
-      "&response_type=code" +
-      "&scope=snsapi_userinfo" +
-      "&state=STATE" +
-      "#wechat_redirect";
-    if (this.getPara().code) {
-      if (window.location.href.indexOf("openId") === -1) {
-        window.location.href = XHRUrl +
-          "?ref=" + localStorage.getItem("currentUrl") +
-          "&response_type=code" +
-          "&scope=snsapi_base" +
-          "&state=pay" +
-          "&code=" + this.getPara().code +
-          "#wechat_redirect";
-      }
-    } else {
-      if (window.location.href.indexOf("openId") !== -1) {
-        let count = sessionStorage.getItem("count");
-        if (!count) {
-          sessionStorage.setItem("count", 1);
-          if (localStorage.getItem("currentUrl") && localStorage.getItem("currentUrl").indexOf("?") != -1) {
-            window.location.href = localStorage.getItem("currentUrl") + "&openId=" + this.getPara().openId;
-          } else {
-            window.location.href = localStorage.getItem("currentUrl") + "?openId=" + this.getPara().openId;
-          }
-          localStorage.setItem("openId", this.getPara().openId);
-        }
-
-      } else {
-        localStorage.setItem("currentUrl", _currentPageUrl);
-        window.location.href = _url;
-      }
-    }
-  };
-
-  //手机号验证
-  mobileCheck() {
-    let _phoneCheckParams = {
-      isValid: 1, //  string  是   1
-      firstResult: 0, //  string  是 分页参数
-      maxResult: 99999, //  string  是 分页参数
-      customerId: '', //  string  是 用户id
-    };
-    //customsId获取（customerId和patientCustomerId同时存在的话取patientCustomerId）
-    if (!api.getPara().openId) {
-      if (api.getPara().patientCustomerId !== undefined) {
-        _phoneCheckParams.customerId = api.getPara().patientCustomerId;
-      } else if (api.getPara().customerId !== undefined) {
-        _phoneCheckParams.customerId = api.getPara().customerId;
-      } else {
-        _phoneCheckParams.customerId = 0;
-      }
-    }
-    if (parseInt(_phoneCheckParams.customerId) > 0) {
-      return true;
-    } else {
-      api.ajax({
-        url: "/mcall/patient/customer/unite/v1/getById/",
-        method: 'POST',
-        data: _phoneCheckParams,
-        beforeSend: function() {},
-        timeOut: 2000,
-        done(data) {
-          localStorage.setItem("customerBaseInfo_one", JSON.stringify(data));
-          if (data && data.responseObject && data.responseObject.responseData && data.responseObject.responseData.dataList) {
-            let _mobile = data.responseObject.responseData.dataList.patientCustomerUnite.mobile;
-            if (_mobile && _mobile.length > 0) {
-              //已绑定手机号
-              return true;
-            } else {
-              //未绑定手机号
-              sessionStorage.setItem("loginBack", window.location.href);
-              window.location.href = '/dist/login.html?customerId=' + _phoneCheckParams.customerId;
-            }
-          }
-        }
-      })
-    }
+    wxCommon.checkOpenId();
   }
 
-  //时间处理  2017年05月08日 星期一 14:20 / 2017.05.08 星期一 14:20
+  //get openID
+  wxGetOpenId() {
+    wxCommon.wxGetOpenId();
+  }
+
+
+//手机号验证
+  mobileCheck() {
+    accountValidate();
+  }
+
+//时间处理  2017年05月08日 星期一 14:20 / 2017.05.08 星期一 14:20
   timeFormate(op) {
     let _operationTime = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
       _timeDeal = op.time.substring(0, 10).replace(/\-/g, "\/"),
@@ -322,7 +184,7 @@ class Api {
       case 3:
         break;
     }
-    return { year: _timeCompare, years: _timeCompares, week: _week, hour: _hours };
+    return {year: _timeCompare, years: _timeCompares, week: _week, hour: _hours};
   }
 
   MillisecondToDate(msd) {
@@ -332,7 +194,7 @@ class Api {
         time = parseInt(time / 60.0) + "分钟"; //+ parseInt((parseFloat(time /60.0) -parseInt(time /60.0)) *60) +"秒";
       } else if (time >= 60 * 60 && time < 60 * 60 * 24) {
         time = parseInt(time / 3600.0) + "小时" + parseInt((parseFloat(time / 3600.0) -
-          parseInt(time / 3600.0)) * 60) + "分钟"; //+
+            parseInt(time / 3600.0)) * 60) + "分钟"; //+
         // parseInt((parseFloat((parseFloat(time /3600.0) - parseInt(time /3600.0)) *60) -
         //     parseInt((parseFloat(time /3600.0) - parseInt(time /3600.0)) *60)) *60) +"秒";
       } else if (time >= 60 * 60 * 24) {
@@ -364,13 +226,7 @@ class Api {
 
   }
 }
-Array.prototype.removeByValue = function(val) {
-  for (let i = 0; i < this.length; i++) {
-    if (this[i] == val) {
-      this.splice(i, 1);
-      break;
-    }
-  }
-};
+
 let api = new Api();
+prototype();
 export default api;
