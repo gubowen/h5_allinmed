ff7f5b9b2de23243ece5251afc7581a13ffb83b1
wangjingrong <wangjingrong@allinmd.cn>
2017-10-10 12:23:14 +0800

    问题：UI微调 reviewer：谷博文

--- src/modules/imSceneDoctor/components/medicalReportDetail.vue
+++ src/modules/imSceneDoctor/components/medicalReportDetail.vue
@@ -254,6 +254,8 @@
               width: rem(120px);
               height: rem(120px);
               margin: 0 auto;
+              box-shadow: 0 6px 12px 0 rgba(0,0,0,0.05);
+              border-radius:50%;
             }
           }
           .tc-baseInfoItemRight {
@@ -312,9 +314,8 @@
         background-color: #ffffff;
         .title {
           padding: rem(40px) 0 rem(40px) rem(30px);
-          border-bottom: 2px solid #F8F8F8;
+          border-bottom: 1px solid #F8F8F8;
           h3 {
-            font-weight: 600;
             color: #666666;;
             @include font-dpr(18px);
             &:before {
@@ -324,7 +325,9 @@
               height: rem(20px);
               margin-right: rem(10px);
               background: #4CD3C6;
-              vertical-align: middle;
+              /*vertical-align: middle;*/
+              margin-top:-(rem(4px));
+              border-radius: 999px;
             }
           }
         }
