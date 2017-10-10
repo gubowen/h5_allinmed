e5d8b9688f6a13e8d98fccea2762763e87be4d72
qiangkailiang <qkl200605251>
2017-10-10 14:22:19 +0800

    医生IM问诊单样式修改 Reviewer：李晨阳

--- src/modules/imSceneDoctor/components/baseIm.vue
+++ src/modules/imSceneDoctor/components/baseIm.vue
@@ -222,7 +222,7 @@
       connectToNim() {
         const that = this;
         this.nim = NIM.getInstance({
-          // debug: true,
+           debug: true,
           appKey: nimEnv(),
           account: this.userData.account,
           token: this.userData.token,
