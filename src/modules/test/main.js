1f314a2ea58dac7aa1593ec4f91f3ef80548108a
jukun <jukun@allinmd.cn>
2017-11-02 10:05:29 +0800

    vue test demo

--- src/modules/test/main.js
+++ src/modules/test/main.js
@@ -8,6 +8,7 @@
  */
 import Vue from 'vue';
 import App from './App';
+import VueRouter from 'vue-router';
 import fastclick from 'fastclick';
 import "babel-polyfill";
 
@@ -20,8 +21,59 @@ class Test {
   }
 
   init() {
+    Vue.use(VueRouter);
+    
+        // this.routerStart();
+        // this.registerRouter();
+    //Vue实例启动
+    const app = new Vue({
+      // router: this.router,
+      render: h => h(App)
+    }).$mount("#test");
     console.log("asdfasdfasdf".includes("asd"));
   }
+  //路由实例化生成
+  // registerRouter() {
+  //   const options={
+  //     duration: '0.3',              //转场动画时长，默认为0.3，单位秒
+  //     firstEntryDisable: false,     //值为true时禁用首次进入应用时的渐现动画，默认为false
+  //     firstEntryDuration: '.6',     //首次进入应用时的渐现动画时长，默认为.6
+  //     forwardAnim: 'fadeInRight',   //前进动画，默认为fadeInRight
+  //     backAnim: 'fadeInRight',       //后退动画，默认为fadeInLeft
+  //     sameDepthDisable: false,      //url深度相同时禁用动画，默认为false
+  //     tabs: [{
+  //       name:'addPatient'
+  //     },{
+  //       name:'selectArea'
+  //     }],                       //默认为[]，name对应路由的name,以实现类似app中点击tab页面水平转场效果，如tab[1]到tab[0]，会使用backAnim动画，tab[1]到tab[2]，会使用forwardAnim动画
+  //     tabsDisable: false,           //值为true时，tabs间的转场没有动画，默认为false
+  //     disable: false,               //禁用转场动画，默认为false，嵌套路由默认为true
+  //   };
+  //   this.router = new VueRouter({
+  //     routes: this.routes,
+  //     scrollBehavior (to, from, savedPosition) {
+  //       return {x: 0, y: 0}
+  //     }
+  //   });
+  //   Vue.use(vueg, this.router,options);
+  // }
+  // //路由注册
+  // routerStart(){
+  //   this.routes=[
+  //     {
+  //       path:'/',
+  //       redirect:"/hello"
+  //     },
+  //     {
+  //       path: '/hello',
+  //       name: "hello",
+  //       component: addPatient,
+  //       meta: {
+  //         keepAlive: true
+  //       },
+  //     },
+  //   ]
+  // }
 }
 
 
