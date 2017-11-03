ac5eb054551b33f7600c805ed617e2624f05da82
lichenyang <lichenyang@allinmd.cn>
2017-11-03 13:48:29 +0800

    问诊单提示话术修改 check in：鞠坤

--- src/modules/imScene/components/medicalReport.vue
+++ src/modules/imScene/components/medicalReport.vue
@@ -34,7 +34,7 @@
           <img src="../../../common/image/imScene/chatting_portrait_system@2x.png" alt="">
         </figure>
         <figcaption class="main-message-content">
-          <p v-if="timeSlot">您好！分诊医生正在详细阅读您提交的资料，将在5分钟内答复，进一步与您沟通疾病细节完善您的就诊病历，并为您推荐对症专家。</p>
+          <p v-if="timeSlot">您好！分诊医生正在详细阅读您提交的资料，将尽快给您答复，并根据情况为您推荐对症专家。</p>
           <p v-else-if="!timeSlot">您好！分诊服务时间为09：00-22：00，如有问题请留言，分诊医生上班后会为您答复。</p>
           <!--<p>①  与您沟通分析病情</p>-->
           <!--<p>②  根据病情推荐对症专家</p>-->
