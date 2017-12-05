d9434573e9b1886a66cda8b8cb6c9dcc7556edae
qiangkailiang <qiangkailiang@allinmd.cn>
2017-12-05 16:34:29 +0800

    Vconsole关闭 打包编译上线 Reviewer：李晨阳

--- src/modules/index/components/slider.vue
+++ src/modules/index/components/slider.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script type="text/ecmascript-6">
-    /**
+/**
      * @Desc：Vue-slider
      * @Usage:
      * @Notify：
@@ -20,175 +20,211 @@
      * Created by qiangkailiang on 2017/7/21.
      */
 
-    function addClass(el, className) {
-        if (hasClass(el, className)) {
-            return;
+function addClass(el, className) {
+  if (hasClass(el, className)) {
+    return;
+  }
+  let newClass = el.className.split(" ");
+  newClass.push(className);
+  el.className = newClass.join(" ");
+}
+
+function hasClass(el, className) {
+  let reg = new RegExp("(^|\\s)" + className + "(\\s|$)");
+
+  return reg.test(el.className);
+}
+
+import BScroll from "better-scroll";
+export default {
+  name: "slider",
+  props: {
+    loop: {
+      type: Boolean,
+      default: true
+    },
+    autoPlay: {
+      type: Boolean,
+      default: true
+    },
+    interval: {
+      type: Number,
+      default: 4000
+    }
+  },
+  data() {
+    return {
+      dots: [],
+      currentPageIndex: 0
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this._setSliderWidth();
+      this._initDots();
+      this._initSlider();
+      if (this.autoPlay) {
+        this._play();
+      }
+    }, 20);
+    window.addEventListener("resize", () => {
+      if (!this.slider || !this.slider.enabled) {
+        return;
+      }
+      clearTimeout(this.resizeTimer);
+      this.resizeTimer = setTimeout(() => {
+        if (this.slider.isInTransition) {
+          this._onScrollEnd();
+        } else {
+          if (this.autoPlay) {
+            this._play();
+          }
         }
-        let newClass = el.className.split(" ");
-        newClass.push(className);
-        el.className = newClass.join(" ");
+        this.refresh();
+      }, 60);
+    });
+  },
+  activated() {
+    if (this.slider) {
+      this.slider.enable();
+      let pageIndex = this.slider.getCurrentPage().pageX;
+      if (pageIndex > this.dots.length) {
+        pageIndex = pageIndex % this.dots.length;
+      }
+      this.slider.goToPage(pageIndex, 0, 0);
+      if (this.loop) {
+        pageIndex -= 1;
+      }
+      this.currentPageIndex = pageIndex;
+      if (this.autoPlay) {
+        this._play();
+      }
     }
-
-    function hasClass(el, className) {
-        let reg = new RegExp('(^|\\s)' + className + '(\\s|$)');
-
-        return reg.test(el.className)
-    }
-
-    import BScroll from "better-scroll";
-    export default{
-        data(){
-            return {
-                dots: [],
-                currentPageIndex: 0
-            }
-        },
-        props: {
-            loop: {
-                type: Boolean,
-                default: true
-            },
-            autoPlay: {
-                type: Boolean,
-                default: true
-            },
-            interval: {
-                type: Number,
-                default: 4000
-            }
-        },
-        mounted(){
-            setTimeout(() => {
-                this._initSlider();
-            }, 20);
-
-            window.addEventListener("resize", () => {
-                if (!this.slider) {
-                    return;
-                }
-
-                this._setSliderWidth(true);
-                this.slider.refresh();
-            })
-        },
-        methods: {
-            _setSliderWidth(isResize){
-                this.children = this.$refs.sliderGroup.children;
-
-                let width = 0;
-                let sliderWidth = this.$refs.slider.clientWidth;
-
-                for (let i = 0; i < this.children.length; i++) {
-                    let child = this.children[i];
-                    addClass(child, "slider-item");
-                    child.style.width = sliderWidth + "px";
-                    width += sliderWidth;
-                }
-
-                if (this.loop && !isResize) {
-                    width += 2 * sliderWidth;
-                }
-
-                this.$refs.sliderGroup.style.width = width + "px";
-                this._initDots();
-            },
-            _initDots(){
-                this.dots = new Array(this.children.length);
-            },
-            _initSlider(){
-                this._setSliderWidth(false);
-                if (this.autoPlay) {
-                    this._play();
-                }
-                this.slider = new BScroll(this.$refs.slider, {
-                    scrollX: true,
-                    scrollY: false,
-                    momentum: false,
-                    snap: true,
-                    snapLoop: this.loop,
-                    snapThreshLoop: 0.3,
-                    snapSpeed: 400
-                });
-
-                this.slider.on("scrollEnd", () => {
-                    let pageIndex = this.slider.getCurrentPage().pageX;
-                    if (this.loop) {
-                        pageIndex -= 1;
-                    }
-
-                    this.currentPageIndex = pageIndex;
-
-                    if (this.autoPlay) {
-                        clearTimeout(this.timeout);
-                        this._play();
-                    }
-                })
-            },
-            _play(){
-                let pageIndex = this.currentPageIndex + 1;
-                if (this.loop) {
-                    pageIndex += 1;
-                }
-
-                this.timeout = setTimeout(() => {
-                    this.slider.goToPage(pageIndex, 0, 400)
-                }, this.interval);
-            }
-        },
-        destoryed(){
-            clearTimeout(this.timeout);
+  },
+  deactivated() {
+    this.slider.disable();
+    clearTimeout(this.timer);
+  },
+  beforeDestroy() {
+    this.slider.disable();
+    clearTimeout(this.timer);
+  },
+  methods: {
+    refresh() {
+      if (this.slider) {
+        this._setSliderWidth(true);
+        this.slider.refresh();
+      }
+    },
+    _setSliderWidth(isResize) {
+      this.children = this.$refs.sliderGroup.children;
+      let width = 0;
+      let sliderWidth = this.$refs.slider.clientWidth;
+      for (let i = 0; i < this.children.length; i++) {
+        let child = this.children[i];
+        addClass(child, "slider-item");
+        child.style.width = sliderWidth + "px";
+        width += sliderWidth;
+      }
+      if (this.loop && !isResize) {
+        width += 2 * sliderWidth;
+      }
+      this.$refs.sliderGroup.style.width = width + "px";
+    },
+    _initSlider() {
+      this.slider = new BScroll(this.$refs.slider, {
+        scrollX: true,
+        scrollY: false,
+        momentum: false,
+        snap: {
+          loop: this.loop,
+          threshold: 0.3,
+          speed: 400
         }
-
+      });
+      this.slider.on("scrollEnd", this._onScrollEnd);
+      this.slider.on("touchend", () => {
+        if (this.autoPlay) {
+          this._play();
+        }
+      });
+      this.slider.on("beforeScrollStart", () => {
+        if (this.autoPlay) {
+          clearTimeout(this.timer);
+        }
+      });
+    },
+    _onScrollEnd() {
+      let pageIndex = this.slider.getCurrentPage().pageX;
+      if (this.loop) {
+        pageIndex -= 1;
+      }
+      this.currentPageIndex = pageIndex;
+      if (this.autoPlay) {
+        this._play();
+      }
+    },
+    _initDots() {
+      this.dots = new Array(this.children.length);
+    },
+    _play() {
+      let pageIndex = this.slider.getCurrentPage().pageX + 1;
+      clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        this.slider.goToPage(pageIndex, 0, 400);
+      }, this.interval);
     }
+  }
+};
 </script>
 
 <style  lang="scss" rel="stylesheet/scss" scoped>
-  @import "../../../../scss/library/_common-modules";
-    .slider {
-        min-height: 1px;
-        .slider-group {
-            position: relative;
-            overflow: hidden;
-            white-space: nowrap;
-            .slider-item {
-                float: left;
-                box-sizing: border-box;
-                overflow: hidden;
-                text-align: center;
-                a {
-                    display: block;
-                    width: 100%;
-                    overflow: hidden;
-                    text-decoration: none;
+@import "../../../../scss/library/_common-modules";
+.slider {
+  min-height: 1px;
+  .slider-group {
+    position: relative;
+    overflow: hidden;
+    white-space: nowrap;
+    .slider-item {
+      float: left;
+      box-sizing: border-box;
+      overflow: hidden;
+      text-align: center;
+      a {
+        display: block;
+        width: 100%;
+        overflow: hidden;
+        text-decoration: none;
 
-                    img {
-                        display: block;
-                        width: 100%;
-                    }
-                }
-            }
+        img {
+          display: block;
+          width: 100%;
         }
-        .dots {
-            position: absolute;
-            right: 0;
-            left: 0;
-            bottom: rem(80px);
-            text-align: center;
-            font-size: 0;
-            .dot {
-                display: inline-block;
-                margin: 0 4px;
-                width: rem(16px);
-                height: rem(16px);
-                border-radius: 50%;
-                background: #E5E5E5;
-                transition: width 0.3s linear;
-                transform-origin: 50% 50%;
-                &.active {
-                    border-radius: 50%;
-                    background: #00D6C6;
-                }
-            }
-        }
+      }
     }
+  }
+  .dots {
+    position: absolute;
+    right: 0;
+    left: 0;
+    bottom: rem(80px);
+    text-align: center;
+    font-size: 0;
+    .dot {
+      display: inline-block;
+      margin: 0 4px;
+      width: rem(16px);
+      height: rem(16px);
+      border-radius: 50%;
+      background: #e5e5e5;
+      transition: width 0.3s linear;
+      transform-origin: 50% 50%;
+      &.active {
+        border-radius: 50%;
+        background: #00d6c6;
+      }
+    }
+  }
+}
 </style>
