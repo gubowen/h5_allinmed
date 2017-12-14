d449d495aaeda7a84ed7a28999198cbb8d2593ff
jukun <jukun@allinmd.cn>
2017-12-14 18:04:12 +0800

    进度条优化 review：李晨阳

--- src/modules/consult/components/progressBar.vue
+++ src/modules/consult/components/progressBar.vue
@@ -21,30 +21,9 @@
 <script type="text/ecmascript-6">
 export default {
   data() {
-    return {
-      imageList: [
-        //        {src:require("../../../common/image/img00/patientConsult/modules.uploadfiles.png")},
-        {
-          src: require("../../../common/image/img00/patientConsult/pictureGuide2_01.jpg")
-        },
-        {
-          src: require("../../../common/image/img00/patientConsult/pictureGuide2_02.jpg")
-        },
-        {
-          src: require("../../../common/image/img00/patientConsult/pictureGuide2_03.jpg")
-        },
-        {
-          src: require("../../../common/image/img00/patientConsult/pictureGuide2_04.jpg")
-        },
-        {
-          src: require("../../../common/image/img00/patientConsult/pictureGuide2_05.jpg")
-        }
-      ]
-    };
+    return {};
   },
-  mounted() {
-    console.log(this.progerssBarParams.progerssParams);
-  },
+  mounted() {},
   acctived() {},
   props: {
     progerssBarParams: {
