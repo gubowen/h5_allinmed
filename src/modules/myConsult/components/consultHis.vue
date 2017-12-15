ba48285fe50f35f533ef2e3e5bd43bf3442b248a
wangjingrong <wangjingrong@allinmd.cn>
2017-12-15 15:32:29 +0800

    问题：打包构建 reviewer：谷博文

--- src/modules/myConsult/components/consultHis.vue
+++ src/modules/myConsult/components/consultHis.vue
@@ -237,7 +237,7 @@
         return result;
       },
       hrefToConsult(){
-        window.location.href = '/dist/consult.html?customerId=' + api.getPara().customerId || localStorage.getItem("userId");
+        window.location.href = `/dist/consult.html?customerId=${localStorage.getItem("userId") || api.getPara().customerId}`;
       },
       hrefToSuggest(opt){
         siteSwitch.weChatJudge(()=>{
