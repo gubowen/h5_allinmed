23ce1f2dc3bd38aed8ac783ff5d61611a78bb231
wangjingrong <wangjingrong@allinmd.cn>
2017-12-19 15:29:24 +0800

    问题：增加验证微信是否绑定手机号 reviewer：谷博文

---
+++ src/common/js/auth/checkBinding.js
@@ -0,0 +1,44 @@
+/**
+ * @Desc：获取微信是否绑定其他账号
+ * @Usage:
+ * @Notify：
+ * @Depend：
+ *
+ * Created by Wangjingrong on 17/12/7.
+ */
+
+import ajax from "common/js/util/ajax";
+
+const XHRList = {
+  checkBinding: "/mcall/patient/customer/unite/v1/getMapList/"
+}
+
+class Checkbinding {
+  constructor() {
+
+  }
+
+  getMessage(phNum) {
+    return new Promise((resolve, reject) => {
+      ajax({
+        url: XHRList.checkBinding,
+        method: "post",
+        data: {
+          mobile:phNum,
+          isValid:1
+        },
+        timeout: 10000,
+        done(res) {
+          if(res&&res.responseObjectresponseData&&res.responseObjectresponseData.dataList){
+            resolve(res.responseObjectresponseData.dataList.uniteFlagWeixin);
+          }else{
+            reject("数据错误");
+          }
+        }
+      })
+    });
+
+  }
+}
+
+export default new Checkbinding();
