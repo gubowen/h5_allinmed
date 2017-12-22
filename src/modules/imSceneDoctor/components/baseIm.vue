ac1da2304b37a0be1d216365895d31b9e1e91718
qiangkailiang <qiangkailiang@allinmd.cn>
2017-12-22 16:47:03 +0800

    个人中心增加手机号修改的判断 Reviewer：李晨阳

--- src/modules/imSceneDoctor/components/baseIm.vue
+++ src/modules/imSceneDoctor/components/baseIm.vue
@@ -61,7 +61,7 @@
             </HospitalNotice>
             <!--文本消息-->
             <ContentText v-if="msg.type==='text' && msg.text" :contentMessage="msg" :userData="userData"
-                         :targetData="targetData">
+                         :targetData="targetData"  @deleteMsgEvent="deleteMsgEvent(msg)" @longTouchEmitHandler="deleteMsgIndex=index">
             </ContentText>
             <!--图像消息-->
             <ImageContent v-if="(msg.type==='file'||msg.type==='image') && msg.file" :imageMessage="msg" :nim="nim"
@@ -252,7 +252,8 @@
         },
         sendTextContent: "",
         footerPosition: "main-input-box",
-        scrollHeight: document.body.scrollTop
+        scrollHeight: document.body.scrollTop,
+        deleteMsgIndex: -1
       };
     },
 
@@ -297,7 +298,91 @@
         this.onFocus = false;
         this.autoSizeTextarea();
       },
-
+      deleteMsgEvent(msg) {
+        // const deleteMsg = new DeleteMsg(this.nim, msg);
+        // const deleteMsgTips = new DeleteMsgTips(
+        //   this.nim,
+        //   this.targetData.account,
+        //   {
+        //     cType: "0",
+        //     cId: this.cId,
+        //     mType: "36",
+        //     conId: this.orderSourceId,
+        //     patientName: this.$store.state.patientName,
+        //     deleteMsg: msg
+        //   }
+        // );
+        const _DeleteTimeLimit = "2分钟";
+        const that = this;
+        this.nim.deleteMsg({
+          msg: msg,
+          done(error) {
+            console.log(error);
+            if (!error) {
+              setTimeout(() => {
+                that.nim.sendCustomMsg({
+                  scene: "p2p",
+                  to: that.targetData.account,
+                  custom: JSON.stringify({
+                    cType: "0",
+                    cId: that.cId,
+                    mType: "36",
+                    conId: that.orderSourceId,
+                    patientName: that.$store.state.patientName,
+                    idClient: msg.idClient
+                  }),
+                  content: JSON.stringify({
+                    type: "deleteMsgTips",
+                    data: {
+                      from: that.$store.state.patientName || "患者",
+                      deleteMsg: msg || {}
+                    }
+                  }),
+                  done(tipsError, tipsMsg) {
+                    console.log("发送撤回消息完成");
+                    console.log(tipsError);
+                    console.log(tipsMsg);
+                    if (!tipsError) {
+                      console.log(tipsError, tipsMsg);
+                      console.log(`撤回消息提示--发送成功`);
+                      that.sendMessageSuccess(tipsError, tipsMsg);
+                    }
+                  }
+                });
+              }, 20);
+            } else {
+              if (parseInt(error.code) === 508) {
+                that.toastTips = `您只能撤回${_DeleteTimeLimit}内的消息`;
+                that.toastShow = true;
+                setTimeout(() => {
+                  that.toastShow = false;
+                }, 2000);
+              }
+            }
+          }
+        });
+        // deleteMsg
+        //   .deleteMessage()
+        //   .then(msg => {
+        //     console.log(99999);
+        //     deleteMsgTips.sendDeleteTips().then((tipsMsg, tipsError) => {
+        //       console.log(tipsMsg, tipsError);
+        //       console.log(`撤回消息提示--发送成功`);
+        //       that.sendMessageSuccess(tipsError, tipsMsg);
+        //     });
+        //   })
+        //   .catch((error, msg) => {
+        //     console.log(error);
+        //     console.log(8888);
+        //     if (parseInt(error.code) === 508) {
+        //       this.toastTips = `您只能撤回${_DeleteTimeLimit}内的消息`;
+        //       this.toastShow = true;
+        //       setTimeout(() => {
+        //         this.toastShow = false;
+        //       }, 2000);
+        //     }
+        //   });
+      },
       connectToNim() {
         const that = this;
         nimEnv().then(nimEnv => {
