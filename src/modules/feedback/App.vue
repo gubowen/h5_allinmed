f1ff90f0530b5b1bbd3cdc24478217b650cc1502
yangyuxi <yangyuxi@allinmd.cn>
2017-12-25 10:30:31 +0800

    New feedback function added
    create by YuxiYang in 2017/12/25

--- src/modules/feedback/App.vue
+++ src/modules/feedback/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <p>11111</p>
+  <section class="main-inner">
+    <section class="main-inner-content">
+      <header class="feedback-header">
+        <h2 class="feedback-header-top">意见反馈</h2>
+        <p class="feedback-header-bottom">如果您在使用过程中有任何问题或建议，请给我们留言，并留下您的联系方式。</p>
+      </header>
+
+      <section class="feedback-content">
+        <section class="feedback-content-form">
+
+        </section>
+      </section>
+    </section>
+  </section>
 </template>
 
 <script type="text/ecmascript-6">
@@ -9,9 +22,9 @@
    * @Notify：
    * @Depend：
    *
-   * Created by JuKun on 2017/8/05.
+   * Created by YuxiYang on 2017/12/25.
    */
-  import feedback from "./components/feedback";
+//  import login from "./contactUs.html";
   import fb from "common/js/third-party/flexible";
   export default{
     data() {
@@ -22,5 +35,35 @@
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
+  @import "../../../scss/library/_common-modules";
+  html,body{
+    width: 100%;
+    height: 100%;
+  }
+  .main-inner {
+    /*width: 100%;*/
+    height: 100%;
+    padding: rem(0px) rem(30px);
 
+  }
+  .main-inner-content {
+    overflow: hidden;
+    position: relative;
+  }
+  .feedback-header{
+    padding-top: rem(50px);
+    padding-left: rem(14px);
+    box-sizing: border-box;
+  }
+  .feedback-header-top{
+    @include font-dpr(22px);
+    color: #555555;
+    font-weight: normal;
+  }
+  .feedback-header-bottom{
+    margin-top: rem(10px);
+    margin-bottom: rem(36px);
+    @include font-dpr(15px);
+    color: #909090;
+  }
 </style>
--- src/modules/feedback/main.js
+++ src/modules/feedback/main.js
@@ -1,14 +1,30 @@
+/**
+ * @Desc：注册 主入口文件
+ * @Usage:
+ * @Notify：
+ * @Depend：
+ *
+ * Created by Qiangkailiang on 17/7/10.
+ */
 import Vue from 'vue';
-import 'static/js/third-party/flexible.js';
-import 'scss/base.scss';
 import App from './App';
-import Router from 'vue-router';
-import routes from "./router/router.config"
-/* eslint-disable no-new */
-Vue.use(Router);
-const router  = new Router(routes);
+import vueg from 'vueg'
+import 'vueg/css/transition-min.css'
+import "static/css/base.css";
+import  "../../common/js/third-party/jweixin-1.0.0.js"
+import api from '../../common/js/util/util';
+import fastclick from 'fastclick';
+fastclick.attach(document.body);
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app');
+class Contact {
+  constructor() {
+    this.init();
+  }
+  init() {
+    //Vue实例启动
+    const app = new Vue({
+      render: h => h(App)
+    }).$mount("#contactUs");
+  }
+}
+let contact = new Contact();
