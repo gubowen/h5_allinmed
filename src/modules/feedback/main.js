a3e21a5d0ddc5c8109f056cc464852b84ca7d8b8
yangyuxi <yangyuxi@allinmd.cn>
2017-12-26 21:19:19 +0800

    All functions added GLHF
    create by YuxiYang in 2017/12/26

--- src/modules/feedback/main.js
+++ src/modules/feedback/main.js
@@ -3,6 +3,7 @@ import App from './App';
 import 'vueg/css/transition-min.css'
 import "static/css/base.css";
 import  "../../common/js/third-party/jweixin-1.0.0.js"
+import "common/js/third-party/flexible";
 class Contact {
   constructor() {
     this.init();
