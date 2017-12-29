91bf04c48cd069c19306d00855b2fd6dc337e28c
wangjingrong <wangjingrong@allinmd.cn>
2017-12-29 13:30:54 +0800

    问题：打包构建 reviewer：谷博文

--- src/common/js/auth/wxBinding.js
+++ src/common/js/auth/wxBinding.js
@@ -20,13 +20,17 @@ class Wxbinding {
     if(!api.getPara().openId){
       new Promise((resolve, reject) => {
         let cId = "";
-        checkLogin.getStatus().then((res)=>{
-          if(res.data.responseObject.responseStatus){
-            resolve(res.data.responseObject.responsePk);
-          }else{
-            resolve(cId);
-          }
-        })
+        if(api.getPara().customerId && api.getPara().customerId != 0){
+          resolve(api.getPara().customerId);
+        }else{
+          checkLogin.getStatus().then((res)=>{
+            if(res.data.responseObject.responseStatus){
+              resolve(res.data.responseObject.responsePk);
+            }else{
+              resolve(cId);
+            }
+          })
+        }
       }).then((data)=>{
         if(data){
           personalInfo.getMessage(data).then((res)=>{
