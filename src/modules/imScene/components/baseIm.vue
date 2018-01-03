264738bcfe211e6960f4359506139a95f459ecbf
lichenyang <lichenyang@allinmd.cn>
2018-01-03 13:16:18 +0800

    分诊完成状态修改 check in：鞠坤

--- src/modules/imScene/components/baseIm.vue
+++ src/modules/imScene/components/baseIm.vue
@@ -1019,13 +1019,13 @@
                 case 2:
                 case 3:
                 case 7:
+                case 8:
                   break;
                 case -1:
                 case 0:
                 case 4:
                 case 5:
                 case 6:
-                case 8:
                 case 9:
                 case 10:
                 case 11:
