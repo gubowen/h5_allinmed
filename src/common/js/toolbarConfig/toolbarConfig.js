490601bf5a49764d1d8d3d6602492c42dd8c718e
qiangkailiang <qiangkailiang@allinmd.cn>
2018-01-04 11:36:07 +0800

    修改工具栏配置参数 Reviewer:李晨阳

--- src/common/js/toolbarConfig/toolbarConfig.js
+++ src/common/js/toolbarConfig/toolbarConfig.js
@@ -10,15 +10,16 @@
 import api from "common/js/util/util";
 
 
-
 export default function getToolBarConfig(){
   const XHRList="/mcall/comm/data/tool/v1/getMapList/";
+  let deviceType=navigator.userAgent.toLowerCase().includes("iphone")?"IOS":"Android";
+
   return new Promise((resolve,reject)=>{
     api.ajax({
       url:XHRList,
       method:"POST",
       data:{
-        deviceType:"WEB",
+        deviceType,
         sortType:1,
         visitSiteId:api.getSiteId()
       },
