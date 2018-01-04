e4ff56abf22677a19f85966a98b13b7e9c796254
qiangkailiang <qiangkailiang@allinmd.cn>
2018-01-04 10:53:52 +0800

    调试手机更换后无法获取数据问题 Reviewer:李晨阳

--- src/components/searchList.vue
+++ src/components/searchList.vue
@@ -205,15 +205,14 @@
         }
       },
       searchEvent(){
-        console.log(api.getByteLen(this.searchText))
         if (this.searchText.length === 0) {
           this.messageList=[];
           this.noResult=false;
           console.log(5)
           return false;
-        } else if (api.getByteLen(this.searchText) >= 30) {
-          this.searchText = api.getStrByteLen(this.searchText, 30);
-          console.log(5)
+        } else if (api.getByteLen(this.searchText) >= 60) {
+          this.searchText = api.getStrByteLen(this.searchText, 60);
+
           this.getMessageList(this.searchText, 0);
         } else {
           clearTimeout(this.searchTimeout);
@@ -221,7 +220,6 @@
             if (this.searchText.trim().length > 0) {
               this.over = false;
               this.getMessageList(this.searchText, 0);
-              console.log(5)
             }
           }, 300);
         }
