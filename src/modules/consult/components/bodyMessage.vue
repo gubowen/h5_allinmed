8c3d1c6af4cf20dc0057711503491f39188e6028
qiangkailiang <qiangkailiang@allinmd.cn>
2018-01-25 17:29:43 +0800

    解决部分安卓机型不支持ArrayBuffer读取事件的问题 Reviewer：强凯亮

--- src/modules/consult/components/bodyMessage.vue
+++ src/modules/consult/components/bodyMessage.vue
@@ -189,7 +189,7 @@
         this.heightContent = params.height;
         this.weightContent = params.weight;
         this.createParams.patientId = params.patientId;
-        this.createParams.customerId = params.customerId;
+        this.createParams.customerId = params.userId;
         this.createParams.illnessHistoryId = params.illnessHistoryId;
         this.createParams.illnessHistory = params.illnessHistory;
         this.createParams.treatmentHospital = params.treatmentHospital;
@@ -198,7 +198,7 @@
         this.createParams.inspectionAttId = params.inspectionAttId;
         this.createParams.takeMedicine = params.takeMedicine;
         this.createParams.complication = params.complication;
-        this.createParams.optionList = params.optionList;
+        this.createParams.optionList = JSON.stringify(params.optionList);
       },
       contentLimit(element, limit) {
         let ranges = /[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/gi;
