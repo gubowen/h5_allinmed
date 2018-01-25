d5ef7bed6dfd859415e6bd1e32f1e9d055042f14
wangjingrong <wangjingrong@allinmd.cn>
2018-01-25 17:40:45 +0800

    问题：打包构建 reviewer：谷博文

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,21 +1,39 @@
 <template>
-  <button class="amed-button-box">
-
-  </button>
+  <button class="amed-button" @click="buttonCallBack">{{buttonText}}</button>
 </template>
 <script type="text/ecmascript-6">
-  export default{
+  export default {
     name: 'amed-button',
-    methods:{
-
+    methods: {
+      buttonCallBack() {
+        this.$emit("buttonCallBack");
+      }
     },
     props: {
-      show:{
-          type:Boolean
+      buttonText: {
+        type: String,
+        default: "下一页"
       }
     }
   }
 </script>
 <style lang="scss" rel="stylesheet/scss">
   @import "../../../scss/library/_common-modules";
+
+  .amed-button {
+    height: rem(100px);
+    line-height: rem(100px);
+    text-align: center;
+    @include font-dpr(20px);
+    &-large {
+      width: 90%;
+      margin: 0 auto;
+    }
+    &-mormal {
+      padding: 0 rem(100px);
+    }
+    &-large {
+      padding: 0 rem(30px);
+    }
+  }
 </style>
--- src/modules/comExample/components/button.vue
+++ src/modules/comExample/components/button.vue
@@ -1,15 +1,17 @@
 <template>
-  <button></button>
+  <buttons></buttons>
 </template>
 <script type="text/ecmascript-6">
-import fb from "common/js/third-party/flexible";
-import api from "common/js/util/util";
-export default {
-  data() {
-    return {};
-  },
-  methods: {}
-};
+  import api from "common/js/util/util";
+  import buttons from "components/button/button";
+  export default {
+    data() {
+      return {};
+    },
+    components: {
+      buttons
+    }
+  };
 </script>
 <style lang="scss" rel="stylesheet/scss">
   @import "../../../../scss/library/_common-modules";
