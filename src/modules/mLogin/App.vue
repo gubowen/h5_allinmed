219b2f7d9c5c47c69ac2f60bedc0380299d236f5
lichenyang <lichenyang@allinmd.cn>
2018-01-26 15:37:25 +0800

    打包构建

--- src/modules/mLogin/App.vue
+++ src/modules/mLogin/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="width: 100%;height: 100%">
+  <div style="width: 100%;height: 100%;background-color: #ffffff;">
       <keep-alive>
         <router-view v-if="$route.meta.keepAlive" style="min-height:100%"></router-view>
       </keep-alive>
