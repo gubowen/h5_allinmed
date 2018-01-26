4be563d4106cc2258b9561c511c8b2f2348fa2a3
yangyuxi <yangyuxi@allinmd.cn>
2018-01-26 13:31:55 +0800

    组件库

--- src/modules/comExample/components/index.vue
+++ src/modules/comExample/components/index.vue
@@ -1,6 +1,9 @@
 <template>
   <ul class="com-exa-list">
     <li><router-link to="/buttons">Buttons</router-link></li>
+    <li><router-link to="/Switch">Switch</router-link></li>
+    <li><router-link to="/MultipleChoice">Multiple Choice</router-link></li>
+    <li><router-link to="/SingleChoice">Single Choice</router-link></li>
   </ul>
 </template>
 <style lang="scss" rel="stylesheet/scss">
@@ -12,8 +15,12 @@
     text-align:center;
     line-height:rem(80px);
     height:rem(80px);
+    border-bottom:1px solid #000000;
     a{
       color:#444;
+    }
+    li{
+
     }
   }
 </style>
